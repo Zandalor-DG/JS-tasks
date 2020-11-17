b8165d39db0bd1a5c31d517334e9622863299ff8
Zandalor-DG <Paliy.zdg@gmail.com>
2020-11-17 16:18:40 +0300

    made the first task

---
+++ js-task-1.js
@@ -0,0 +1,9 @@
+// const myPow = (number, exp) => {
+//     if (exp === 1) {
+//         return number;
+//     } else {
+//         return number * myPow(number, exp - 1);
+//     }
+// };
+
+// console.log(myPow(3, 3));
