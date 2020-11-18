d337ee519b87dfbc18f666767bc2421f7f9087f7
Zandalor-DG <Paliy.zdg@gmail.com>
2020-11-18 11:45:46 +0300

    Last edits

--- js-task-1.js
+++ js-task-1.js
@@ -60,26 +60,35 @@
 //task#3
 
 function digToText(dig) {
-    let words = {
+    let result;
+    const words = {
         m3: [
-            ['тысяча', 'тысячи', 'тысяч']
+            ["тысяча", "тысячи", "тысяч"]
         ],
-        m2: ['сто', 'двести', 'триста', 'четыреста', 'пятьсот', 'шестьсот', 'семьсот', 'восемьсот', 'девятьсот'],
-        m1: ['дестять', 'двадцать', 'тридцать', 'сорок', 'пятьдесят', 'шестьдесят', 'семьдесят', 'восемдесят', 'девяносто'],
-        m0: ['один', 'два', 'три', 'четыре', 'пять', 'шесть', 'семь', 'восемь', 'девять', 'десять'],
-        f0: ['одна', 'две'],
-        l0: ['дестять', 'одиннадцать', 'двенадцать', 'тринадцать', 'четырнадцать', 'пятнадцать', 'шестнадцать', 'семнадцать', 'восемнадцать', 'девятнадцать']
+        m2: ["сто", "двести", "триста", "четыреста", "пятьсот", "шестьсот", "семьсот", "восемьсот", "девятьсот"],
+        m1: [
+            "дестять", "двадцать", "тридцать", "сорок", "пятьдесят", "шестьдесят", "семьдесят", "восемдесят",
+            "девяносто"
+        ],
+        m0: ["один", "два", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять", "десять"],
+        f0: ["одна", "две"],
+        l0: [
+            "дестять", "одиннадцать", "двенадцать", "тринадцать", "четырнадцать", "пятнадцать", "шестнадцать",
+            "семнадцать", "восемнадцать", "девятнадцать"
+        ]
     };
-    let dim = function(dig, power, words) {
-        let result = '';
-        let pow = Math.floor(dig / Math.pow(10, power)) % Math.pow(10, 3);
-        if (!pow) return result;
-        let n2 = Math.floor(pow / 100);
-        let n1 = Math.floor(pow % Math.pow(10, 2) / 10);
-        let n0 = Math.floor(pow % 10);
-        let s1 = (n1 > 0) ? ' ' : '';
-        let s0 = (n0 > 0) ? ' ' : '';
-        let get_n = function() {
+    const dim = function(dig, power, words) {
+        result = "";
+        const pow = Math.floor(dig / Math.pow(10, power)) % Math.pow(10, 3);
+        if (!pow) {
+            return result;
+        }
+        const n2 = Math.floor(pow / 100);
+        const n1 = Math.floor(pow % Math.pow(10, 2) / 10);
+        const n0 = Math.floor(pow % 10);
+        const s1 = (n1 > 0) ? " " : "";
+        const s0 = (n0 > 0) ? " " : "";
+        const get_n = function() {
             switch (power) {
                 case 0:
                 case 6:
@@ -101,32 +110,36 @@ function digToText(dig) {
         if (n1 > 0) {
             if (n1 > 1) {
                 result += s1 + words.m1[n1 - 1];
-                if (n0 > 0) get_n();
+                if (n0 > 0) {
+                    get_n();
+                }
             } else {
                 result += s1 + words.l0[n0];
             }
         } else {
-            if (n0 > 0) get_n();
+            if (n0 > 0) {
+                get_n();
+            }
         }
         if (power) {
-            var d = (power - 3) / 3;
+            const d = (power - 3) / 3;
             if ((d == 0) && (n0 + n1 * 10 >= 11 && n0 + n1 * 10 <= 14)) {
-                result += ' ' + words.m3[0][2];
+                result += ` ${words.m3[0][2]}`;
             } else if (n0 == 1) {
-                result += ' ' + words.m3[d][0];
+                result += ` ${words.m3[d][0]}`;
             } else if ((n0 >= 2) && (n0 <= 4)) {
-                result += ' ' + words.m3[d][1];
+                result += ` ${words.m3[d][1]}`;
             } else if ((n0 == 0) || (n0 >= 5 && n0 <= 9)) {
-                result += ' ' + words.m3[d][2];
+                result += ` ${words.m3[d][2]}`;
             }
         }
         return result;
-    }
-    result = '';
+    };
+    result = "";
     for (let i = 9; i > -1; i -= 3) {
-        result += dim(dig, i, words) + ' ';
+        result += dim(dig, i, words) + " ";
     }
-    return result.replace(/[\s]{2,}/ig, ' ').trim();
+    return result.replace(/[\s]{2,}/ig, " ").trim();
 }
 
-console.log(digToText(179992));
+console.log(digToText(987456));
